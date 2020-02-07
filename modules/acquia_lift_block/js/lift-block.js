ae85ddf516e76c3bc37f88cdbd74f4a5707ec45d
Ron Northcutt <ron.northcutt@acquia.com>
2020-02-07 13:28:25 -0600

    Using the lift event listener to be sure we are doing the visibility stuff *after* Lift is ready to go

--- modules/acquia_lift_block/js/lift-block.js
+++ modules/acquia_lift_block/js/lift-block.js
@@ -5,27 +5,30 @@
 
   "use strict";
 
+ 
+
   /**
    * Changes the visibility of a lift block based on segments.
    */
   Drupal.behaviors.liftBlocks = {
     attach: function (context) {
-      let liftEnabled = typeof AcquiaLift === 'object';
-      console.log("I should always see this!");
-      if (liftEnabled) {
+
+      // Wait until Lift has finished finding out whats going on
+      window.addEventListener('acquiaLiftStageCollection', function (e) {
         let liftSegmentsExist = typeof AcquiaLift.currentSegments === 'object';
         if (liftSegmentsExist) {
+          // Loop through each current segment and look for blocks to show
           Object.values(AcquiaLift.currentSegments).forEach(value => {
             let liftclass = 'liftblock-' + value.id;
             let blocks = document.getElementsByClassName(liftclass);
-            Object.keys(blocks).forEach(key=>{
-               //blocks[key].style.display = "block";
-               blocks[key].style.borderTop = "25px solid red";
-               console.log(value.id);
+            // Find every block that is marked for this segment, and show it
+            Object.keys(blocks).forEach(key => {
+              blocks[key].style.display = "inherit";
             });
           });
         }
-      }
+      }); // End eventListener
+
     }
   };
 
